7e08eeb3ea2258d2f390b27e2a769ed3b792d64b
Suraj Nair <surajnair9698@gmail.com>
2025-08-27 23:23:38 +0530

    chore: setup initial project structure and configure webpack for HTML

---
+++ modules/initial-page-load.js
@@ -0,0 +1,13 @@
+export default function () {
+  const theDiv = document.querySelector("#content");
+
+  let heading = document.createElement("h1");
+  heading.textContent = "Welcome To The Grid";
+
+  let hero = document.createElement("div");
+  hero.classList.add("hero");
+
+  hero.appendChild(heading);
+
+  theDiv.appendChild(hero);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import intialPage from "../modules/initial-page-load";
+import "./styles.css";
 console.log("hello world");
+intialPage();
